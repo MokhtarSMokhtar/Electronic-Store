cedbaa2b4b295cff0186de7c858225cc87a478ce
mekhaaaa <70482286+mekhaaaa@users.noreply.github.com>
2022-11-21 13:55:42 +0200

    s

--- Electronic-Store/src/app/app.module.ts
+++ Electronic-Store/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogInComponent } from './Component/log-in/log-in.component';
+import { HeaderComponent } from './Component/log-in/header/header.component';
+import { LoginComponent } from './Component/login/login.component';
+import { FooterComponent } from './Component/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    HeaderComponent,
+    LoginComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
